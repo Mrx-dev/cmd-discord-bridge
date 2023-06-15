34d090a411c5029ce31aa8ccdfe49b61686e54a0
Mrx-dev <mrxdevreply@gmail.com>
2023-06-15 10:54:43 +0300

    Fixing Path

--- src/handlers/eventHandler.ts
+++ src/handlers/eventHandler.ts
@@ -1,9 +1,9 @@
 import * as fs from 'fs';
 import { EventsType } from '../types/EventsType'
 import { Client, Message } from 'discord.js';
-
+import * as path from 'node:path';
 export let eventHandler = (client: Client) => {
-    fs.readdirSync('./events').forEach((file) => {
+    fs.readdirSync(path.join(__dirname, '../events')).forEach((file) => {
         if (!file.endsWith('.ts')) return;
         let event: EventsType = require(`../events/${file}`).event;
         if (event.once) return client.once(event.name, (client: Client) => event.execute(client));
